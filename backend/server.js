221b7cd3ada5ce574a98812684dcdbdc37bcc31f
lixuling <antonisaputra049@gmail.com>
2022-10-08 12:54:04 +0700

    please work 5

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config()
 getMongo()
-app.use(cors())
+
 app.get("/", (req, res) => {
   res.send("API is running")
 })
@@ -24,30 +24,10 @@ const server = app.listen(
   PORT,
   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://awok-talking.netlify.app/');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://awok-talking.netlify.app/",
-    methods: ["GET", "POST"],
+    origin: "https://awok-talking.netlify.app",
     allowedHeaders: ["my-custom-header"],
     credentials: true
   },
