be8c107076c6bcf93f14113249c5bfaf2a130da2
lixuling <antonisaputra049@gmail.com>
2022-10-08 12:16:27 +0700

    please work

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,24 @@ app.use(cors())
 app.get("/", (req, res) => {
   res.send("API is running")
 })
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://awok-talking.netlify.app/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use("/api/user",userRouter)
 app.use("/api/chat", chatRouter)
 app.use("/api/message",messageRouter)
